741584dc9332de4bfeba071ea4127b55d941bfb5
lizeyang1995 <424011527@qq.com>
2020-11-18 23:06:30 +0800

    [lizeyang] feat: add method two for problem2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,42 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoSpringConfigApplicationTests {
+	@Autowired
+	MockMvc mockMvc;
+	@Autowired
+	private LevelController levelController;
 
 	@Test
-	void contextLoads() {
+	void should_return_basic_when_levelNumber_less_than_1() throws Exception {
+		levelController.setLevelNumber(0);
+		mockMvc.perform(get("/level"))
+				.andExpect(jsonPath("$", Matchers.is("basic")));
+	}
+
+	@Test
+	void should_return_basic_when_levelNumber_large_than_1() throws Exception {
+		levelController.setLevelNumber(2);
+		mockMvc.perform(get("/level"))
+				.andExpect(jsonPath("$", Matchers.is("advanced")));
+	}
+
+	@Test
+	void should_return_basic_when_levelNumber_is_1() throws Exception {
+		levelController.setLevelNumber(1);
+		mockMvc.perform(get("/level"))
+				.andExpect(jsonPath("$", Matchers.is("advanced")));
 	}
 
 }
